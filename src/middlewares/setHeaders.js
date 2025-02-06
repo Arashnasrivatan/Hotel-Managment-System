5e973bdbc7709408ace78947a527c6f6c8c36dfa
Arashnasrivatan <arashnasrivatan@gmail.com>
2025-02-06 21:32:29 +0330

    fix: correct header name for CORS authorization

--- src/middlewares/setHeaders.js
+++ src/middlewares/setHeaders.js
@@ -3,7 +3,7 @@ const express = require("express");
 exports.setHeaders = (req = express.request, res = express.response, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Method", "GET, POST, PUT, DELETE, PATCH");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authoritarian");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   next();
 };
